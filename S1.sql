6799657234530659c7f367f0ecd0c25eed5610c0
Teerapat Chantra <basteerapat06a1@gmail.com>
2025-07-17 17:14:46 +0700

    Update S1.sql

--- S1.sql
+++ S1.sql
@@ -1,38 +1,53 @@
-SELECT EmployeeID , FirstName , LastName
- FROM Employees
- SELECT *
+SELECT EmployeeID, FirstName, LastName
+FROM Employees;
+GO
 
+SELECT *
 FROM Employees
-WHERE City = 'London'
+WHERE City = 'London';
+GO
 
 SELECT *
-
 FROM Products
+WHERE UnitPrice BETWEEN 50 AND 100;
+GO
 
-WHERE UnitPrice BETWEEN 50 AND 100
 SELECT *
-
 FROM Customers
-
 WHERE Country IN ('Brazil','Argentina','Mexico');
+GO
+
 SELECT *
-
 FROM Employees
+WHERE FirstName LIKE 'N%';
+GO
 
-WHERE FirstName LIKE 'N%'
 SELECT *
-
 FROM Employees
+WHERE FirstName LIKE '___'; 
+GO
 
-WHERE FirstName LIKE '_ _ _ _ _'
-SELECT ProductID,ProductName,UnitPrice
-
+SELECT ProductID, ProductName, UnitPrice
 FROM Products
-
-ORDER BY UnitPrice DESC
+ORDER BY UnitPrice DESC;
+GO
 
 SELECT CompanyName, ContactName
-
 FROM Customers
+ORDER BY ContactName ASC;
+GO
 
-ORDER BY ContactName ASC
+SELECT FirstName, LastName
+FROM Employees
+WHERE FirstName LIKE '%an%';
+GO
+
+SELECT CompanyName
+FROM Customers
+WHERE CompanyName LIKE '_A%';
+GO
+
+SELECT ProductName, UnitPrice, UnitsInStock
+FROM Products
+ORDER BY UnitsInStock DESC;
+GO
